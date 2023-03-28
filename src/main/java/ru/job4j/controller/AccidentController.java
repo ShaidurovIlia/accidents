f068defe75f9b5f0953b263c2cf6e6b07128eb33
Admin <shaidurov.ilha@yandex.ru>
2023-03-28 14:14:50 +0200

    @ModelAttribute. Создание инцидента. @RequestParam. Форма редактирования.

---
+++ src/main/java/ru/job4j/controller/AccidentController.java
@@ -0,0 +1,40 @@
+package ru.job4j.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.job4j.model.Accident;
+import ru.job4j.service.AccidentService;
+
+import java.util.Optional;
+
+@Controller
+@AllArgsConstructor
+public class AccidentController {
+    private final AccidentService accidents;
+
+    @GetMapping("/createAccident")
+    public String viewCreateAccident() {
+        return "createAccident";
+    }
+
+    @PostMapping("/saveAccident")
+    public String save(@ModelAttribute Accident accident) {
+        accidents.create(accident);
+        return "redirect:/index";
+    }
+
+    @PostMapping("/formUpdateAccident")
+    public String update(Model model, @RequestParam("id") int id) {
+        Optional<Accident> optAccident = accidents.findById(id);
+        if (optAccident.isEmpty()) {
+            return "404";
+        }
+        model.addAttribute("accident", optAccident.get());
+        return "formUpdateAccident";
+    }
+}
--- src/main/java/ru/job4j/repository/AccidentMem.java
+++ src/main/java/ru/job4j/repository/AccidentMem.java
@@ -5,6 +5,7 @@ import ru.job4j.model.Accident;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,7 +15,7 @@ public class AccidentMem {
 
     private final AtomicInteger ids = new AtomicInteger(3);
 
-    public AccidentMem () {
+    public AccidentMem() {
         accidents.put(1, new Accident(1, "John Smith", "Description 1", "Address 1"));
         accidents.put(2, new Accident(2, "Sam Brown", "Description 2", "Address 2"));
         accidents.put(3, new Accident(3, "Paul Pierce", "Description 3", "Address 3"));
@@ -22,5 +23,22 @@ public class AccidentMem {
 
     public List<Accident> findAll() {
         return accidents.values().stream().toList();
+    }
+
+    public void create(Accident accident) {
+        accident.setId(ids.incrementAndGet());
+        accidents.put(accident.getId(), accident);
+    }
+
+    public void update(Accident accident) {
+        accidents.replace(accident.getId(), accident);
+    }
+
+    public Optional<Accident> findById(int id) {
+        Accident accident = accidents.get(id);
+        if (accident == null) {
+            return Optional.empty();
+        }
+        return Optional.of(accident);
     }
 }
