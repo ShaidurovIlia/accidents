05499b8d33e9cc6bce1d035030af68906246b299
Admin <shaidurov.ilha@yandex.ru>
2023-04-05 14:37:41 +0200

    Spring mock. Тесты на редактирование и создание моделей в контроллерах.

--- src/test/java/ru/job4j/controller/RegControllerTest.java
+++ src/test/java/ru/job4j/controller/RegControllerTest.java
@@ -1,15 +1,22 @@
 package ru.job4j.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.Main;
+import ru.job4j.model.User;
+import ru.job4j.service.UserService;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -21,6 +28,9 @@ public class RegControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     @WithMockUser
     public void regPage() throws Exception {
@@ -28,5 +38,21 @@ public class RegControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenRegUserThenReturnDefaultMessage() throws Exception {
+        User user = new User();
+        when(userService.create(user)).thenReturn(true);
+        this.mockMvc.perform(post("/reg")
+                        .param("username", "Name")
+                        .param("password", "123456"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/login"));
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(userService).create(argument.capture());
+        assertThat(argument.getValue().getUsername()).isEqualTo("Name");
     }
 }
